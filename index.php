7dcdaf702b212b0e2475cf86abb273535e678d23
jannerch <jannamaemacatangay5@gmail.com>
2024-12-01 18:07:57 +0800

    importing homepage

---
+++ index.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Janna Macatangay</title>
+  <link rel="icon" type="image/x-icon" href="pictures-potfolio/cute logo.png" />
+  <link>
+  <link rel="stylesheet" href="portfolio-style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+</head>
+<style>
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
+
+<body>
+  <div class="card text-center">
+    <div class="card-header">
+      <div class="full-width-header">
+        <header class="header-container container-fluid">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-12 col-md-auto d-flex justify-content-center p-0">
+              <a class="navbar-brand" href="#">
+                <img src="pictures-potfolio/cute logo.png" alt="Logo" class="img-fluid">
+              </a>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-md d-flex flex-column align-items-center text-center p-0"
+                style="margin-right: 80px; padding-left: 15px;">
+                <h1 class="card-title"><b>Janna Macatangay</b></h1>
+                <p class="card-text"><b>Developer | Web Designer</b></p>
+              </div>
+            </div>
+          </div>
+        </header>
+
+
+        <div class="card-body">
+          <ul class="nav justify-content-center" style="gap: 50px;">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#home-section">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about-section">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#project-section">Projects</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+  <!-- Home -->
+  <section>
+    <div id="home-section">
+      <div class="row mt-5 justify-content-center">
+        <div class="col-md-6 d-flex flex-column align-items-start justify-content-center intro-section fade-in p-0">
+          <div>
+            <h1 class="firstName">Janna </h1>
+            <h1 class="lastName">Macatangay</h1>
+            <p class="intro-text slide-in" style="color: #E67D7D;">Developer | Web Designer</p>
+          </div>
+          <div class="icon mt-1 d-flex justify-content-start flex-wrap p-0">
+            <a class="ms-2" href="https://www.facebook.com/profile.php?id=100090524644747">
+              <img src="pictures-potfolio/fb.png" alt="Facebook icon" width="24" height="24" />
+            </a>
+            <a class="ms-2" href="https://www.instagram.com/juana_mae/">
+              <img src="pictures-potfolio/ig.png" alt="Instagram icon" width="24" height="24" />
+            </a>
+            <a class="ms-2" href="https://www.linkedin.com/in/janna-macatangay-19b885295/">
+              <img src="pictures-potfolio/li.png" alt="LinkedIn icon" width="24" height="24" />
+            </a>
+            <a class="ms-2" href="https://github.com/JannaMacatangay">
+              <img src="pictures-potfolio/github.png" alt="GitHub icon" width="24" height="24" />
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4 d-flex justify-content-center transparent p-0">
+          <img src="pictures-potfolio/img-jannerch.png" alt="Janna-image" class="img-jannerch" />
+        </div>
+      </div>
+  </section>
+
+  <!-- About -->
+  <section>
+    <div id="about-section" class="container-fluid" style="background-color: #222;">
+      <div class="row mt-5 justify-content-center">
+        <div class="col-12 col-md-4 d-flex justify-content-center transparent p-0">
+          <img src="pictures-potfolio/jannerch-pic-about.png" alt="Janna-about-image" class="jannerch-pic-about" />
+        </div>
+        <div class="col-12 col-md-8 d-flex align-items-center intro fade-in">
+          <div>
+            <h1 class="about" style="font-weight: bolder; color: #E67D7D; font-size: 64px; padding-left: 15px;">About Me
+            </h1>
+            <p class="intro slide-in" style="color: #ffffff; font-size: 18px; text-align: justify; padding-left: 15px;">
+              Hello! I’m Janna, a Bachelor of Science in Information Technology (BSIT) student at the
+              <span class="PUP" style="color: #f04848;">Polytechnic University of the Philippines - Sto. Tomas
+                Campus.</span>
+            </p>
+            <p class="intro slide-in" style="color: #ffffff; font-size: 18px; text-align: justify; padding-left: 15px;">
+              As a dedicated student leader, I’m passionate about fostering a vibrant, supportive academic community. I
+              aim to create meaningful experiences for fellow students, contribute actively to our organization’s
+              initiatives, and continuously develop my technical and leadership skills. I am driven to bridge knowledge
+              gaps and support academic growth within our community, aspiring to make a positive impact both in and out
+              of the society.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Projects -->
+  <section>
+    <div id="project-section" class="container-fluid">
+      <div class="row mt-5 justify-content-center">
+        <div class="col-12 text-center p-0">
+          <h1 class="project" style="font-weight: 900; color: #E67D7D; font-size: 64px;">
+            Projects
+          </h1>
+        </div>
+      </div>
+
+      <div class="row mt-5 justify-content-center">
+        <div
+          class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center intro-section fade-in order-1 order-md-1 p-0">
+          <div class="btn-group-vertical" role="group" aria-label="Vertical button group"
+            style="width: 100%; display: flex; justify-content: center; align-items: center;">
+            <a href="#" target="_blank" class="btn-link">
+              <button type="button" class="btn btn-primary mb-3"
+                style="font-size: 24px; background-color: #222; color: #E67D7D;">INSERT PROJECT</button>
+            </a>
+           
+          </div>
+        </div>
+
+        <div
+          class="col-12 col-md-6 d-flex justify-content-center align-items-center transparent order-2 order-md-2 p-0">
+          <div class="row justify-content-center">
+            <img src="pictures-potfolio/logo-project.png" alt="logo-project1" class="logo-project"
+              style="max-width: 100%; height: auto;">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+  <!-- footer -->
+  <div class="container-fluid">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center p-0">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+          <svg class="bi" width="30" height="24">
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Janna Macatangay</span>
+      </div>
+    </footer>
+  </div>
+
+
+
+
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
